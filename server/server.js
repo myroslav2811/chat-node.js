210631faafc16dc9a6910b080060f30394fea9b9
myroslav1812 <myroslav1812@gmail.com>
2020-01-15 21:04:18 +0200

    Connected to DB

--- server/server.js
+++ server/server.js
@@ -2,27 +2,53 @@ const express = require('express');
 const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
 
 const port = 3000;
+const dbName = 'chat';
+const client = new MongoClient('mongodb://localhost:27017', {useUnifiedTopology: true});
 
-app.use(express.static('../view'));
+client.connect(err => {
+    assert.equal(null, err);
+    console.log('Connected to server...');
+    app.use(express.static('../view'));
+    const db = client.db(dbName);
 
-io.on('connection', socket => {
-    console.log(`new user connected ${socket.id}`);
-    socket.on('disconnect', () => {
-        console.log(`user ${socket.id} disconnected`);
+    io.on('connection', socket => {
+        console.log(`new user connected ${socket.id}`);
+
+        db.collection('messages').find().toArray((err, res) => {
+            assert.equal(null, err);
+            socket.emit('receive all messages', res);
+        });
+
+        socket.on('disconnect', () => {
+            console.log(`user ${socket.id} disconnected`);
+        });
+
+        //
+
+        socket.on('send message', msg => {
+            const obj = {
+                author: socket.id.substr(1, 4),
+                message: msg,
+                date: new Date()
+            };
+            db.collection('messages').insertOne(obj, err => {
+                assert.equal(null, err);
+                io.emit('receive message', obj);
+                console.log('success');
+            });
+
+
+        })
     });
 
-    socket.on('send message', msg => {
-        const obj = {
-          author: socket.id.substr(1, 4),
-            message: msg,
-            date: new Date()
-        };
-        io.emit('receive message', obj)
-    })
+    server.listen(port, () => {
+        console.log(`listening on port ${port}`);
+    });
+
 });
 
-server.listen(port, () => {
-    console.log(`listening on port ${port}`);
-});
+
--- view/js/main.js
+++ view/js/main.js
@@ -1,28 +1,27 @@
 window.addEventListener('load', () => {
 
+    const socket = io();
+
     const appendMessage = msg => {
         const li = document.createElement('li');
         li.innerHTML = `${msg.author}: ${msg.message} (${msg.date})`;
         document.getElementById('message-list').appendChild(li);
     };
 
-    fetch('./messages.json')
-        .then(data => data.json())
-        .then(data => {
-            data.messages.forEach(appendMessage);
-        });
-
-    const socket = io();
-
     const sendMessageHandler = () => {
-        const msg = document.getElementById('message-input').value;
-        socket.emit('send message', msg);
+        let msg = document.getElementById('message-input');
+        socket.emit('send message', msg.value);
+        msg.value = '';
     };
-
-    socket.on('receive message', appendMessage);
 
     document.getElementById('send-message').addEventListener('click', sendMessageHandler);
 
+    socket.on('receive all messages', arr => {
+        console.log(arr);
+        arr.forEach(appendMessage);
+    });
+
+    socket.on('receive message', appendMessage);
 
 });
 
